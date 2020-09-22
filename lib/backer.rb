832694cb6c1f356fe2843a0e49349a24fe9ac4f3
Dasha Lary <dk2800@nyu.edu>
2020-09-22 22:25:59 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -8,8 +8,9 @@ def initialize(name)
 end
   
 def back_project(project)
-project = Project.new(title)
+project = Project.new(@title)
 @backed_projects << project 
+project.backers << self
 backer = Backer.new(@name)
 @backers << backer 
 end 
